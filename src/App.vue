ec1f1861f27f9501247612e30b2b8c6854478757
troynina <troy333nina@gmail.com>
2020-02-02 20:16:05 +0300

    divider added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,38 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id = "app">
+    <v-navigation-drawer
+      color = "green"
+      dark
+      expand-on-hover
+      hide-overlay
+      permanent
+      right
+      app
+    >
+    <v-list
+      nav 
+      shaped
+      dense
+    >
+      <v-list-item two-line>
+        <v-list-item-avatar>
+          <img :src = "'https://randomuser.me/api/portraits/men/1.jpg'">
+        </v-list-item-avatar>
+
+        <v-list-item-content class = "text-left">
+          <v-list-item-title class = "font-weight-black">SocialLink</v-list-item-title>
+          <v-list-item-subtitle>Иван Иванов</v-list-item-subtitle> 
+        </v-list-item-content>
+      <v-list-item>
+      <v-divider class = "my-3"></v-divider>   
+    </v-list>
+    </v-navigation-drawer>
+    <v-content class="px-12 py-3">
+      <v-container fluid>
+        <router-view/>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <style lang="scss">
